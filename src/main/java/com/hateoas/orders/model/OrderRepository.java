1dbf7132c2d80a53f5cd77ad66466fb4f799796c
Rasheed <shaikrasheed3014@gmail.com>
2022-11-10 22:42:24 +0530

    Add. Order repository

--- src/main/java/com/hateoas/orders/model/OrderRepository.java
+++ src/main/java/com/hateoas/orders/model/OrderRepository.java
@@ -1,2 +1,15 @@
-package com.hateoas.orders.model;public interface OrderRepository {
+package com.hateoas.orders.model;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface OrderRepository extends CrudRepository<Order, Integer> {
+    @Query(value = "SELECT * FROM orders WHERE customer_id = :id", nativeQuery = true)
+    List<Order> findByCustomerId(@Param("id") int id);
+
+    @Query(value = "SELECT * FROM orders WHERE product_id = :id", nativeQuery = true)
+    List<Order> findByProductId(@Param("id") int id);
 }
--- src/test/java/com/hateoas/orders/model/OrderRepositoryTest.java
+++ src/test/java/com/hateoas/orders/model/OrderRepositoryTest.java
@@ -1,2 +1,78 @@
-package com.hateoas.orders.model;public class OrderRepositoryTest {
+package com.hateoas.orders.model;
+
+import com.hateoas.customers.model.Customer;
+import com.hateoas.products.model.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class OrderRepositoryTest {
+    @Autowired
+    private OrderRepository orderRepository;
+    private Order ironmanOrdersIphone;
+    private List<Order> orders;
+    private Customer ironman;
+    private Product iPhone;
+
+    @BeforeEach
+    void setUp() {
+        ironman = new Customer("Ironman");
+        Customer thor = new Customer("Thor");
+        iPhone = new Product("iPhone", 80000);
+        Product macBook = new Product("MacBook", 200000);
+        int quantity = 2;
+        ironmanOrdersIphone = new Order(ironman, iPhone, quantity, quantity * iPhone.getPrice());
+        Order ironmanOrdersMacBook = new Order(ironman, macBook, quantity, quantity * macBook.getPrice());
+        Order thorOrdersIphone = new Order(thor, iPhone, quantity, quantity * iPhone.getPrice());
+        Order thorOrdersMacBook = new Order(thor, macBook, quantity, quantity * macBook.getPrice());
+        orders = Arrays.asList(ironmanOrdersIphone, ironmanOrdersMacBook, thorOrdersIphone, thorOrdersMacBook);
+        orderRepository.saveAll(orders);
+    }
+
+    @Test
+    void shouldBeAbleToSaveOrder() {
+        Order savedOrder = orderRepository.save(ironmanOrdersIphone);
+
+        assertEquals(savedOrder.getId(), ironmanOrdersIphone.getId());
+        assertEquals(savedOrder.getCustomer().getId(), ironmanOrdersIphone.getCustomer().getId());
+        assertEquals(savedOrder.getProduct().getId(), ironmanOrdersIphone.getProduct().getId());
+        assertEquals(savedOrder.getCost(), ironmanOrdersIphone.getCost());
+    }
+
+    @Test
+    void shouldBeAbleToGetOrderDetailsById() {
+        Order orderById = orderRepository.findById(ironmanOrdersIphone.getId()).get();
+
+        assertEquals(orderById.getCustomer().getName(), ironmanOrdersIphone.getCustomer().getName());
+        assertEquals(orderById.getProduct().getId(), ironmanOrdersIphone.getProduct().getId());
+        assertEquals(orderById.getProduct().getName(), ironmanOrdersIphone.getProduct().getName());
+        assertEquals(orderById.getCost(), ironmanOrdersIphone.getCost());
+    }
+
+    @Test
+    void shouldBeAbleToGetOrdersOfACustomer() {
+        List<Order> customerOrders = orderRepository.findByCustomerId(ironman.getId());
+
+        assertEquals(2, customerOrders.size());
+        assertEquals("Ironman", customerOrders.get(0).getCustomer().getName());
+        assertEquals("iPhone", customerOrders.get(0).getProduct().getName());
+    }
+
+    @Test
+    void shouldBeAbleToGetOrdersOfAProduct() {
+        List<Order> productOrders = orderRepository.findByProductId(iPhone.getId());
+
+        assertEquals(2, productOrders.size());
+        assertEquals("Ironman", productOrders.get(0).getCustomer().getName());
+        assertEquals("Thor", productOrders.get(1).getCustomer().getName());
+    }
 }
