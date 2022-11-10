b3ecb81c93a305528ae3f5f9a2691ecd345d0683
Rasheed <shaikrasheed3014@gmail.com>
2022-11-10 17:18:33 +0530

    Add. Product entity

---
+++ src/main/java/com/hateoas/products/model/Product.java
@@ -0,0 +1,50 @@
+package com.hateoas.products.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private float price;
+
+    public Product() {
+    }
+
+    public Product(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "id=" + id + ", name='" + name + '\'' + ", price=" + price + '}';
+    }
+}
